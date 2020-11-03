5ec78e7cbd927023674d726992c15d2005833740
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-11-03 16:34:10 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ echo -e "\n $ANSI_YELLOW *** testing docker run - fedora *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Display current directory: $ANSI_RESET"
 docker run -i --rm --name some_fedora quay.io/ibmz/fedora-s390x:34 echo "hello world!"
-docker stop some fedora
+docker stop some_fedora
 
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
