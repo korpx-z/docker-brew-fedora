8fde3222ee5fa1371e466dac8131a1b7680b1623
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-11-03 16:32:07 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ export ANSI_YELLOW="\e[1;33m"
 export ANSI_GREEN="\e[32m"
 export ANSI_RESET="\e[0m"
 
-echo -e "\n $ANSI_YELLOW *** testing docker run - alpine *** $ANSI_RESET \n"
+echo -e "\n $ANSI_YELLOW *** testing docker run - fedora *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Display current directory: $ANSI_RESET"
 docker run -i --rm --name some_fedora quay.io/ibmz/fedora-s390x:34 echo "hello world!"
