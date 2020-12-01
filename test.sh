ce8817612a5698ee4658437c2fbf8c51dd09ff46
charles-korpics <charles.korpics@ibm.com>
2020-12-01 18:40:25 -0500

    32

---
+++ test.sh
@@ -0,0 +1,13 @@
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** testing docker run - fedora *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW Display current directory: $ANSI_RESET"
+
+docker run -i --rm --name fedora quay.io/ibmz/fedora-s390x:32 echo "hello world!"
+
+echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
